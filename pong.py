075637ebee465588f73c4aa0cdb3c82a7ff962cc
Mahi <47994802+Mahisaurus-Rex@users.noreply.github.com>
2019-08-15 17:48:11 -0400

    did stuff

--- pong.py
+++ pong.py
@@ -1,11 +1,13 @@
 import arcade 
 import time
 import random
+from chars import *
 
 # Constants
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 SCREEN_TITLE = "Arcade Pong"
+
 
 class Pong(arcade.Window):
     """
@@ -21,13 +23,42 @@ class Pong(arcade.Window):
 
     def setup(self):
         """ Set up the game here. Call this function to restart the game. """
+        #set initial variables
+        p1_x,p1_y=30,30
+        p2_x,p2_y=(screen.get_width()-60),30
+        ball_x,ball_y=((screen.get_width())/2),((screen.get_height())/2)
+        slope_x,slope_y=2,2
+        #set constants
+        paddle_width = screen.get_width()/64
+        paddle_height = screen.get_height()/3.6
+
+        #set initial conditions
+        p1_score,p2_score=0,0
+        p1=Paddle(p1_x,p1_y)
+        p2=Paddle(p2_x,p2_y)
+        ball=Ball(ball_x,ball_y)
+
         pass
 
     def on_draw(self):
         """ Render the screen. """
 
+
         arcade.start_render()
         # Code to draw the screen goes here
+
+    def on_key_press(self, key, modifiers):
+        #controls
+        #p1
+        if pressed[pygame.K_w]:
+            p1.up()
+        if pressed[pygame.K_s]:
+            p1.down()
+        #p2
+        if pressed[pygame.K_UP]:
+            p2.up()
+        if pressed[pygame.K_DOWN]:
+            p2.down()
 
 
 def main():
